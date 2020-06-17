6b692dec552ca49323c883f39858ef758b1ef134
Haroon Zaman <haroonzaman80@gmail.com>
2020-06-17 23:42:26 +0500

    update logo and text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div >
-      Hello World 11
+      Hello World
     </div>
   );
 }
